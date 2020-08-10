d89fcdf06f7e43feed5b6cb6c45cba3985ae2063
João Dinis Ferreira <jdinster@gmail.com>
2020-08-10 16:19:06 +0200

    Document run_server

--- run_server.py
+++ run_server.py
@@ -1,3 +1,10 @@
+""" Simple script to run a production-grade version of the microservice implementation
+
+Can be run by calling:
+python run_server.py
+
+"""
+
 from gevent.pywsgi import WSGIServer
 from app.count_unique_ips import app
 
