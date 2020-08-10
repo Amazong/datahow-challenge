e305a72b54819172a901f1f01fcce710bd53fb3f
João Dinis Ferreira <jdinster@gmail.com>
2020-08-10 15:41:04 +0200

    documentation

--- app/count_unique_ips.py
+++ app/count_unique_ips.py
@@ -3,18 +3,22 @@ import json
 from flask import Flask, redirect, url_for, request, render_template
 
 # Alternative server
-#from waitress import serve
+# from waitress import serve
 
 app = Flask(__name__)
 
 unique_ips = set()
 
 def parse_json(json_req):
+   """Adds the IP from a received request to the set of previously-seen IPs"""
+
    json_req = json.loads(json_req)
    unique_ips.add(json_req['ip'])
    return json_req['ip']
 
 def unique_ip_count_as_json():
+   """Returns the total number of unique IPs as a JSON-formatted string"""
+
    unique_ip_count = {
       "unique_ips": len(unique_ips)
    }
@@ -23,10 +27,18 @@ def unique_ip_count_as_json():
 
 @app.route('/')
 def main():
+   """Placeholder text for the server root"""
+
     return "Hello, DataHow!"
 
 @app.route('/logs', methods = ['POST', 'GET'])
 def receive_json():
+   """Implementation of /logs.
+   
+   This listens for incoming POST requests on port :5000.
+   When a request is received, the incoming JSON is parsed using the function parse_json.
+   """
+
    if request.method == 'POST':
       return parse_json(request.data)
    else:
@@ -34,7 +46,13 @@ def receive_json():
 
 @app.route('/visitors')
 def get_unique_ip_count():
+   """Implementation of /visitors.
+   
+   This returns a single JSON object containing:
+   {"unique_ips", # of unique IPs seen}
+   """
    return unique_ip_count_as_json()
+
 
 if __name__ == '__main__':
    app.run(debug=True, port=5000, threaded=True)
